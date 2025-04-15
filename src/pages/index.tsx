91de41092a529ad759b7b01267588a7846f09208
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-15 10:37:08 -0300

    feat: add Table component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,18 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+
+  const clientes = [
+
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Matheus', 23, '3'),
+    new Cliente('Larissa', 20, '4')
+
+  ]
+
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -8,7 +20,7 @@ export default function Home() {
       text-white`}>
 
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela clientes={clientes}></Tabela>
       </Layout>
     </div>
   )
